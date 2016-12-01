990c4a390cd4c9702f8d8c99524de8d247633158
student <a@a.com>
2016-11-30 17:53:32 -0800

    Added stuff for beginning of lab 12
    company get info and school get info

--- mysqlresumes/WebAppStuff.sql
+++ mysqlresumes/WebAppStuff.sql
@@ -1,3 +1,5 @@
+use richardson;
+
 create or replace view school_view as
 select s.*, a.street, a.zip_code from school s
 join address a on a.address_id = s.address_id;
@@ -9,3 +11,28 @@ left join account a on a.account_id = r.account_id;
 
 select * from resume_view;
 
+#lab 12
+
+ DROP PROCEDURE IF EXISTS company_getinfo;
+ DELIMITER //
+ CREATE PROCEDURE company_getinfo (_company_id int)
+ BEGIN
+ SELECT * FROM company WHERE company_id = _company_id;
+ SELECT a.*, s.company_id FROM address a
+ LEFT JOIN company_address s on s.address_id = a.address_id AND company_id = _company_id;
+ END //
+ DELIMITER ;
+ # Call the Stored Procedure
+ CALL company_getinfo (4);
+ 
+ DROP PROCEDURE IF EXISTS school_getinfo;
+ DELIMITER //
+ CREATE PROCEDURE school_getinfo (school_id int)
+ BEGIN
+ SELECT * FROM school WHERE school_id = school_id;
+ SELECT a.*, school_id FROM address a
+ LEFT JOIN school s on s.address_id = a.address_id;
+ END //
+ DELIMITER ;
+ # Call the Stored Procedure
+ CALL school_getinfo (4);
